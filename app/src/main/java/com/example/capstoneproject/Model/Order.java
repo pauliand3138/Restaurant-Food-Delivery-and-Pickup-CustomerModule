c929fe01a71db9546d02abfd16611e442f090b21
Paul <paul.iand3138@hotmail.com>
2021-09-19 16:50:10 +0800

    Created order type page and validation (address != null) and (price != 0) when user submit their order to firebase

--- app/src/main/java/com/example/capstoneproject/Model/Order.java
+++ app/src/main/java/com/example/capstoneproject/Model/Order.java
@@ -1,53 +1,82 @@
 package com.example.capstoneproject.Model;
 
-public class Order {
-    private String ProductId;
-    private String ProductName;
-    private String Quantity;
-    private String Price;
+import java.util.List;
 
+public class Order {
+    private String custID;
+    private String custTelNo;
+    private String custName;
+    private String orderAddress;
+    private String orderType;
+    private String orderPrice;
+    private List<CartDetail> foods; //List of food order
 
     public Order() {
     }
 
-    public Order(String productId, String productName, String quantity, String price) {
-        ProductId = productId;
-        ProductName = productName;
-        Quantity = quantity;
-        Price = price;
-
+    public Order(String custID, String custTelNo, String custName, String orderAddress, String orderType, String orderPrice, List<CartDetail> foods) {
+        this.custID = custID;
+        this.custTelNo = custTelNo;
+        this.custName = custName;
+        this.orderAddress = orderAddress;
+        this.orderType = orderType;
+        this.orderPrice = orderPrice;
+        this.foods = foods;
     }
 
-    public String getProductId() {
-        return ProductId;
+    public String getCustID() {
+        return custID;
     }
 
-    public void setProductId(String productId) {
-        ProductId = productId;
+    public void setCustID(String custID) {
+        this.custID = custID;
     }
 
-    public String getProductName() {
-        return ProductName;
+    public String getCustTelNo() {
+        return custTelNo;
     }
 
-    public void setProductName(String productName) {
-        ProductName = productName;
+    public void setCustTelNo(String custTelNo) {
+        this.custTelNo = custTelNo;
     }
 
-    public String getQuantity() {
-        return Quantity;
+    public String getCustName() {
+        return custName;
     }
 
-    public void setQuantity(String quantity) {
-        Quantity = quantity;
+    public void setCustName(String custName) {
+        this.custName = custName;
     }
 
-    public String getPrice() {
-        return Price;
+    public String getOrderAddress() {
+        return orderAddress;
     }
 
-    public void setPrice(String price) {
-        Price = price;
+    public void setOrderAddress(String orderAddress) {
+        this.orderAddress = orderAddress;
     }
 
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(String orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public List<CartDetail> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<CartDetail> foods) {
+        this.foods = foods;
+    }
 }
